d16143ec16ba5f3efd26e25dd06aabe85aa3f853
abinthomas258 <abin.thomas@ibsplc.com>
2022-08-08 18:02:20 +0530

    Restructure Routes

---
+++ src/assets/svg/calendar.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+export function Calendar({}) {
+  return (
+    <svg
+      className="h-full"
+      version="1.1"
+      id="Capa_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      width="20px"
+      height="20px"
+      viewBox="0 0 33 33"
+      xmlSpace="preserve"
+    >
+      <g>
+        <g>
+          <path
+            d="M28.5,0h-24C2.019,0,0,2.019,0,4.5v24C0,30.98,2.019,33,4.5,33h24c2.48,0,4.5-2.02,4.5-4.5v-24C33,2.019,30.98,0,28.5,0z
+			 M31.5,28.5c0,1.648-1.35,3-3,3h-24c-1.65,0-3-1.352-3-3v-24c0-1.649,1.35-3,3-3h24c1.65,0,3,1.351,3,3V28.5z"
+          />
+          <circle cx="9.25" cy="4.773" r="1.273" />
+          <circle cx="16.5" cy="4.773" r="1.273" />
+          <circle cx="23.75" cy="4.773" r="1.273" />
+          <rect x="11.134" y="12.902" width="4.596" height="4.088" />
+          <rect x="17.271" y="12.902" width="4.595" height="4.088" />
+          <rect x="23.404" y="12.902" width="4.596" height="4.088" />
+          <rect x="5" y="18.359" width="4.595" height="4.086" />
+          <rect x="11.134" y="18.359" width="4.596" height="4.086" />
+          <rect x="17.271" y="18.359" width="4.595" height="4.086" />
+          <rect x="23.404" y="18.359" width="4.596" height="4.086" />
+          <rect x="5" y="23.816" width="4.595" height="4.086" />
+          <rect x="11.134" y="23.816" width="4.596" height="4.086" />
+          <rect x="17.271" y="23.816" width="4.595" height="4.086" />
+          <rect x="23.404" y="23.816" width="4.596" height="4.086" />
+        </g>
+      </g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+    </svg>
+  )
+}
---
+++ src/setup/routes/routes.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import ErrorPage from '../../pages/errorPages/errorPage'
+import HotelSearch from '../../pages/hotel/search/components/hotelSearch'
+import SignIn from '../../pages/sign-in/SignIn'
+import SignUp from '../../pages/sign-up/SignUp'
+
+function routes() {
+  return (
+    <Routes>
+      <Route path="/" element={<HotelSearch />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  )
+}
+
+export default routes
