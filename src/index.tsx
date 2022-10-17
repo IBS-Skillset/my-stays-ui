5957715a6c6b217203227aeb94599e76f9e94bb4
apeethambaran <v-apeeth@expedia.com>
2022-10-17 15:50:22 +0530

    code refractoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import { Provider } from 'react-redux'
-import { store } from './store'
-import { BrowserRouter } from 'react-router-dom'
 import { I18nextProvider } from 'react-i18next'
-import './style.scss'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import i18nConf from './config/i18nConfig'
+import { store } from './store'
+import './style.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <I18nextProvider i18n={i18nConf.i18next}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </I18nextProvider>,
+  <React.StrictMode>
+    <I18nextProvider i18n={i18nConf.i18next}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </I18nextProvider>
+  </React.StrictMode>,
 )
