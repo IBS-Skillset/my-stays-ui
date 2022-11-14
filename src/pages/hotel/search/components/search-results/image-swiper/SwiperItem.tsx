f881c9b663a50f658f7e5cbe7e0069b7d4a8d84a
Abhijith Ramankutty <a-8625@ibsplc.com>
2022-11-14 13:38:38 +0530

    Updated swiper - new UI

--- src/pages/hotel/search/components/search-results/image-swiper/SwiperItem.tsx
+++ src/pages/hotel/search/components/search-results/image-swiper/SwiperItem.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SwiperItem.css'
 import { SwiperItemType } from './types'
+import ImageNA from '../../../../../../assets/images/Error.jpg'
 
 export type Props = SwiperItemType
 
 function SwiperItem({ imageSrc }: Props) {
+  const [imgSrc, setImgSrc] = useState<string | undefined>(imageSrc)
+  const onError = () => setImgSrc(ImageNA)
   return (
     <li className="swiper-item">
-      <img src={imageSrc} alt="" className="swiper-img" draggable={false} />
+      <img
+        src={imgSrc}
+        alt=""
+        onError={onError}
+        className="swiper-img"
+        draggable={false}
+      />
     </li>
   )
 }
