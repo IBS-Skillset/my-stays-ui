624e44e1013619761db5a6554c5a30361cf32388
Keerthika Govindaraj <a-10420@ibsplc.com>
2022-11-14 14:58:38 +0530

    [TECH-510] Code Review Changes for HotelSearchService

--- src/services/hotel/HotelSearchService.ts
+++ src/services/hotel/HotelSearchService.ts
@@ -15,6 +15,11 @@ class HotelSearchService {
         longitude: hotelAvailabilityRequest.longitude,
         checkInDate: format(hotelAvailabilityRequest.checkInDate, 'yyyyMMdd'),
         checkOutDate: format(hotelAvailabilityRequest.checkOutDate, 'yyyyMMdd'),
+        languageCode:'FRE',
+        countryCode:'FR',
+        occupancy: 1,
+        roomCount: 1
+
       }),
       {
         headers: {
