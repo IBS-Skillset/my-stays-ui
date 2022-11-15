6fac1d6b5fcf0edae989346a02129864b9968164
ArjunAruz <a-10375@ibsplc.com>
2022-11-15 17:53:09 +0530

    Custom router removal

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import i18nConf from './config/i18nConfig'
-import CustomRouter from './setup/routes/CustomRouter'
 import { store } from './store'
 import './style.scss'
-import history from './models/actionModels/history'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18nConf.i18next}>
       <Provider store={store}>
-        <CustomRouter history={history}>
+        <BrowserRouter>
           <App />
-        </CustomRouter>
+        </BrowserRouter>
       </Provider>
     </I18nextProvider>
   </React.StrictMode>,
