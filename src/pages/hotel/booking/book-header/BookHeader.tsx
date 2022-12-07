b2f2acc62b830ce98752c63aa87e117d24c83b2c
Keerthika Govindaraj <a-10420@ibsplc.com>
2022-12-07 15:48:46 +0530

    [TECH-518] Header color change for Final confirmation

--- src/pages/hotel/booking/book-header/BookHeader.tsx
+++ src/pages/hotel/booking/book-header/BookHeader.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { ImCheckmark } from 'react-icons/im'
+import { useLocation } from 'react-router'
 import './BookHeader.scss'
 
 function BookHeader() {
+  const finalConfirmationMatch = useLocation().pathname == '/finalConfirmation'
   return (
     <div className="book-header-container">
       <div className="book-items display-content">
@@ -15,12 +17,27 @@ function BookHeader() {
           <div className="book-indicator progress-divider">2</div>
           <div className="title display-content">Payment Information</div>
         </div>
-        <div className="progress-divider-final border-divider"></div>
+        <div
+          className={
+            'progress-divider-final border-divider ' +
+            (finalConfirmationMatch ? 'progress-divider-after-book' : '')
+          }
+        ></div>
         <div className="items-icon">
-          <div className="book-indicator-icon display-content progress-divider-final">
+          <div
+            className={
+              'book-indicator-icon display-content ' +
+              (finalConfirmationMatch ? 'items-icon-after-book' : '')
+            }
+          >
             <ImCheckmark />
           </div>
-          <div className="title-nonactive display-content">
+          <div
+            className={
+              'title-nonactive display-content ' +
+              (finalConfirmationMatch ? 'booking-confirm-after-book' : '')
+            }
+          >
             Booking is confirmed
           </div>
         </div>
