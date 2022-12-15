847607ba3c0bb4d0be8d0879137c7a22f041b937
Abhishek Ravindran <v-aravindran@expediagroup.com>
2022-12-15 16:47:11 +0530

    my trips integration

--- src/services/trips/MyTripsService.ts
+++ src/services/trips/MyTripsService.ts
@@ -1,16 +1,10 @@
-/*import axios from 'axios'*/
-// eslint-disable-next-line jest/no-mocks-import
-import myTripsResponse from '../../__mocks__/myTripsResponse'
-import { MyTripsResponse } from '../../models/trips/myTripsResponse'
+import axios from 'axios'
 
-//const MY_TRIPS_BASE_URL = `http://${process.env.DOMAIN}:${process.env.HOTEL_SERVICE_PORT}/hotel-book-service/api/trips`
+const MY_TRIPS_BASE_URL = `http://${process.env.DOMAIN}:${process.env.HOTEL_SERVICE_PORT}/book-query/api/myTrips`
 
 class MyTripsService {
   getMyTrips() {
-    /*return axios.get(MY_TRIPS_BASE_URL)*/
-    return new Promise<MyTripsResponse>((resolve) => {
-      resolve(myTripsResponse.trips)
-    })
+    return axios.get(MY_TRIPS_BASE_URL)
   }
 }
 
