b2219e4aff8c3f12a16da6bfd883070540797c59
bkalarikkal <v-bkalarikkal@Traveldoo.com>
2023-04-18 22:22:13 +0530

    [AWS-DEPLOYMENT] - corrections in redirect url

--- src/setup/oauth2/constants/AuthConstants.ts
+++ src/setup/oauth2/constants/AuthConstants.ts
@@ -1,17 +1,17 @@
-let HOST = (window as any).AUTH_SERVER_URI + '/auth-server'
-let HOST_UI = window.location.host
+let HOST_UI = 'http://' + window.location.host
+let HOST_AUTH = 'http://' + window.location.host + '/auth-server'
 if (process.env.NODE_ENV === 'development') {
   HOST = `${process.env.AUTH_SERVER_URI}/auth-server`
 }
 export default {
   CLIENT_ID: 'client',
   SECRET_KEY: 'secret',
-  REDIRECT_URI: `http://${HOST_UI}/authorized`,
-  AUTHORIZE_URL: `${HOST}/oauth2/authorize?`,
-  TOKEN_URL: `${HOST}/oauth2/token?`,
-  LOGIN_URL: `${HOST}/perform_login?`,
-  REVOKE_URL: `${HOST}/oauth2/revoke?`,
-  LOGOUT_URL: `${HOST}/perform_logout?`,
+  REDIRECT_URI: `${HOST_UI}/authorized`,
+  AUTHORIZE_URL: `${HOST_AUTH}/oauth2/authorize?`,
+  TOKEN_URL: `${HOST_AUTH}/oauth2/token?`,
+  LOGIN_URL: `${HOST_AUTH}/perform_login?`,
+  REVOKE_URL: `${HOST_AUTH}/oauth2/revoke?`,
+  LOGOUT_URL: `${HOST_AUTH}/perform_logout?`,
   ERROR_SIGN_IN: 'Invalid Credentials. Please Sign in again!',
   LOGOUT_MESSAGE: 'User logout. Please Sign in again!',
   SESSION_OUT_MESSAGE: 'Session has expired. Please Sign in again!',
